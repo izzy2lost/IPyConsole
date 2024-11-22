a93455f3aaa3936d60c61173f35bbd62faa08eec
吴冰 <wubing@xuanit.com>
2024-11-22 17:52:55 +0800

    add source data bindings

--- ControlLib/Calculate.xaml.cs
+++ ControlLib/Calculate.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ControlLib.Utils;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,14 +22,36 @@ namespace ControlLib
     /// </summary>
     public partial class Calculate : UserControl
     {
+        internal IDictionary<int, Player> Players { get; set; } = new Dictionary<int, Player>();
+
+        internal Player player { get; set; }
+
         public Calculate()
         {
             InitializeComponent();
+
+            player = new Player(0, "cc");
+
+            player.Contents[0] = new string("mm");
+
+            this.DataContext = player;
         }
 
         private void Btn_Click(object sender, RoutedEventArgs e)
         {
             this.tb_1.Text = "enter";
+
+            player.Name = "dd";
+
+            player[0] = new string("nn");
+        }
+
+
+        public void Add_Player()
+        {
+            Player player = new Player(0, "aa");
+
+            Players.Values.Add(player);
         }
     }
 }
---
+++ ControlLib/Utils/Player.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlLib.Utils
+{
+    internal class Player: INotifyPropertyChanged
+    {
+        private readonly int _id;
+
+        private string _name;
+
+        private object[] _contents = new object[10];
+
+        public int Id => _id;
+
+        public string Name
+        {
+            get { return _name; }
+            set 
+            { 
+                _name = value; 
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public object[] Contents
+        {
+            get { return _contents; }
+            set
+            {
+                if (value.Length == 10)
+                {
+                    _contents = value;
+                    OnPropertyChanged(nameof(Contents));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Contents 索引器
+        /// </summary>
+        public object this[int index]
+        {
+            get 
+            {
+                return Contents[index];
+            }
+            set 
+            {
+                Contents[index] = value;
+                OnPropertyChanged(nameof(Contents));
+            }
+        }
+
+        public Player() { }
+
+        public Player(int id, string name)
+        {
+            _id = id;
+            Name = name;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+}
